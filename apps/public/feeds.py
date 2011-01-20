19de88e35712d29af20df2089cf8661caa09611b
mitnk <whgking@gmail.com>
2011-01-20 21:32:07 +0800

    ADD pubdate to rss.

--- apps/public/feeds.py
+++ apps/public/feeds.py
@@ -4,15 +4,18 @@ from apps.public.models import Article
 class LatestEntriesFeed(Feed):
     title = "mitnk's blog"
     link = "http://mitnk.com"
-    description = "Updates on changes and additions to chicagocrime.org."
+    description = "Updates on mitnk.com"
     description_template = "description.html"
 
     def items(self):
-        return Article.objects.order_by('-added')[:5]
+        return Article.objects.order_by('-added')[:10]
 
     def item_title(self, item):
         return item.title
 
     def item_description(self, item):
         return item.content
+
+    def item_pubdate(self, item):
+        return item.added
 
