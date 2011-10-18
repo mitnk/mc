3fb4d8a36ae373813a537c1d1c0adf421ec88f12
mitnk <whgking@gmail.com>
2011-10-18 13:55:53 +0800

    Change get_or_create to get or 404.

--- webapps/news/views.py
+++ webapps/news/views.py
@@ -6,7 +6,6 @@ from common.utils import get_soup_by_url
 from webapps.news.models import News
 
 def send_url_to_kindle(url, title=""):
-    return
     soup = get_soup_by_url(url)
     content = soup.find("div", {"class": "entry-content"})
     if content:
@@ -31,17 +30,13 @@ def index(request):
         if 'http' not in tag['href']:
             tag['href'] = "http://news.ycombinator.com/" + tag['href']
 
-        if 60 < points < 70:
-            send_url_to_kindle(tag['href'], title=tag.string)
-
-        # make sure string not too long to save
-        if len(tag['href']) > 512 or len(tag.string) > 512:
-            continue
-
-        news, created = News.objects.get_or_create(url=tag['href'])
-        if created:
+        try:
+            news = News.objects.get(url=tag['href'])
             news.title = tag.string
             news.points = points
+            news.save()
+        except News.DoesNotExist:
+            news = News(url=tag['href'], points=points, title=tag.string)
             news.save()
         count += 1
 
