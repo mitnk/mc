63c6902ef044c9a2f15ad9b7523862d960713f58
mitnk <whgking@gmail.com>
2011-11-04 17:10:15 +0800

    HN: Add search in CMS.

--- webapps/news/admin.py
+++ webapps/news/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from webapps.news.models import News, Archive
 
-admin.site.register(News)
-admin.site.register(Archive)
+
+class NewsAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'url']
+
+class ArchiveAdmin(admin.ModelAdmin):
+    search_fields = ['file_name', 'url']
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Archive, ArchiveAdmin)
