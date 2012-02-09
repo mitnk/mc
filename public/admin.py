0cc4485ab232991c31c2b5a2e82cf3b1ea868873
mitnk <whgking@gmail.com>
2012-02-09 10:18:18 +0800

    add list_filters into admin site.

--- public/admin.py
+++ public/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 class ArticleAdmin(admin.ModelAdmin):
     search_fields = ['title', 'slug']
     list_display = ("title", "added")
+    list_filter = ('added', 'category')
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category)
--- wiki/admin.py
+++ wiki/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 class ArticleAdmin(admin.ModelAdmin):
     fields = ('title', 'content', 'category')
     list_display = ('title', 'added', 'category')
+    list_filter = ('category', )
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category)
