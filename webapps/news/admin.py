da904c2156f2c3c2a98c9b2283ddd2261de7c743
mitnk <whgking@gmail.com>
2012-02-20 05:56:43 +0800

    update admin.py(2)

--- webapps/news/admin.py
+++ webapps/news/admin.py
@@ -4,12 +4,12 @@ from webapps.news.models import News, Archive
 
 class NewsAdmin(admin.ModelAdmin):
     search_fields = ['title', 'url']
-    list_display = ("title", "url", "points", "added")
+    list_display = ("title", "points", "added")
     list_filter = ('added',)
 
 class ArchiveAdmin(admin.ModelAdmin):
     search_fields = ['file_name', 'url']
-    list_display = ("file_name", "url", "added")
+    list_display = ("file_name", "added")
     list_filter = ('added',)
 
 admin.site.register(News, NewsAdmin)
