a25c7f090845742f538ffa47aa9cd710a68b030e
mitnk <whgking@gmail.com>
2012-02-23 15:48:05 +0800

    move self stuff to self. 增加正交性设计

--- webapps/news/models.py
+++ webapps/news/models.py
@@ -1,4 +1,64 @@
+import datetime
+import logging
+import os
+from urllib2 import URLError
+
 from django.db import models
+from django.conf import settings
+from django.utils.encoding import smart_str
+
+from utils.briticle import Briticle
+
+logger = logging.getLogger("HackerNews")
+if not logger.handlers:
+    handler = logging.FileHandler(settings.LOG_BRITICLE)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s\n%(message)s\n')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+
+class NewsManager(models.Manager):
+    def update_news(self, article_list):
+        """
+        Update News records:
+        1. Add news when it doesnot exist
+        2. Update points when changed
+        3. Update datetime when saving to file
+        """
+        count_logged = count_filed = 0
+        for article in article_list:
+            try:
+                news = self.get(url=article['url'])
+                news.points = article['points']
+                news.save()
+            except self.model.DoesNotExist:
+                news = self.model(url=article['url'],
+                    points=article['points'],
+                    title=smart_str(article['title'])
+                )
+                news.save()
+                count_logged += 1
+
+            # Save articles to file whose points big enough
+            if article['points'] >= settings.POINTS_LIMIT_TO_KINDLE and (not news.filed):
+                try:
+                    br = Briticle(news.url)
+                except Exception, e:
+                    if isinstance(e, URLError) or 'timed out' in str(e):
+                        logger.info("URLError or Time out Exception: %s URL: %s" % (e, news.url))
+                        continue
+                    raise
+
+                mobi = br.save_to_file(settings.HACKER_NEWS_DIR, title=news.title)
+                if mobi:
+                    news.filed = True
+                    news.added = datetime.datetime.now()
+                    news.save()
+                    count_filed += 1
+                else:
+                    logger.error("Failed to save file. URL: %s" % news.url)
+        return count_logged, count_filed
 
 class News(models.Model):
     url = models.CharField(max_length=512)
@@ -6,6 +66,8 @@ class News(models.Model):
     points = models.IntegerField(default=0)
     added = models.DateTimeField(auto_now_add=True)
     filed = models.BooleanField(default=False)
+
+    objects = NewsManager()
 
     class Meta:
         ordering = ["-added"]
--- webapps/news/views.py
+++ webapps/news/views.py
@@ -1,104 +1,36 @@
 import os
-import re
-import time
 from urllib2 import URLError
-from tempfile import NamedTemporaryFile
 
 from django.conf import settings
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.encoding import smart_str
 
-from common.utils import get_soup_by_url, write_to_file
-from webapps.news.models import News
+from webapps.news.models import News, logger
 from webapps.tools import send_mail
 
 from utils.briticle import Briticle
 from utils.hacker_news import HackerNews
 
-import logging
-logger = logging.getLogger("BRITICLE")
-if not logger.handlers:
-    handler = logging.FileHandler(settings.LOG_BRITICLE)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s\n%(message)s\n')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.INFO)
-
 
 def send_to_kindle(request):
     send_to = settings.KINDLE_SENDING_LIST
-    subject = "Hacker News Update"
+    subject = "Docs of HackerNews from mitnk.com"
     files = os.listdir(settings.HACKER_NEWS_DIR)
     if not files:
         return HttpResponse("No new articles filed")
     files = [os.path.join(settings.HACKER_NEWS_DIR, x) for x in files if (x.endswith('.mobi') or x.endswith(".txt"))]
-    text = "%s files sent.\n" % len(files)
-    send_mail(send_to, subject, text, files=files)
-    for f in files:
-        try:
-            os.remove(f)
-        except OSError:
-            pass
-    return HttpResponse(text)
-
-
-def save_to_file(url, dir_name=settings.HACKER_NEWS_DIR, title=None):
-    def clean_dir_files(dir_name):
-        # Remove all middle-files except .MOBI
-        file_list = os.listdir(dir_name)
-        for f in file_list:
-            if not f.endswith(".mobi") and not f.endswith(".txt"):
-                try:
-                    os.remove(dir_name + "/" + f)
-                except OSError:
-                    pass
-        
     try:
-        br = Briticle(url)
-        if not br.content:
-            logger.info("No content found at url: %s" % url)
-            return None
-
-        page_title, content = br.title, br.content
-        if not title:
-            title = page_title
-        file_name = re.sub(r'[^0-9a-zA-Z _-]+', '', title).replace(' ', '_') or 'blank_name'
-        mobi_name = "%s.mobi" % file_name
-
-        file_path = br.save_to_files(file_name, dir_name, title=title)
-        time.sleep(0.3)
-
-        if not file_path or not os.path.exists(file_path):
-            logger.info('File not found for URL: %s' % url)
-            return None
-
-        cmd = "kindlegen %s -o %s > /dev/null" % (file_path, mobi_name)
-        os.system(cmd)
-        time.sleep(1) # Wait a second before checking the file
-        mobi_file = re.sub(r'\.html$', '.mobi', file_path)
-        txt_file = re.sub(r'\.html$', '.txt', file_path)
-
-        clean_dir_files(dir_name)
-        if not os.path.exists(mobi_file):
-            if os.path.exists(txt_file):
-                return txt_file
-            logger.info("Failed to generate mobi file. URL: %s" % url)
-            return None
-
-        try:
-            os.remove(txt_file)
-        except OSError:
-            pass
-
-        return mobi_file
+        info = "%s files sent.\n" % len(files)
+        send_mail(send_to, subject, info, files=files)
     except Exception, e:
-        if isinstance(e, URLError) or 'timed out' in str(e):
-            logger.info("URLError or Time out Exception: %s URL: %s" % (e, url))
-        else:
-            logger.info("*** Exception: %s URL: %s" % (e, url))
-        return None
+        info = "send_mail() failed."
+        logger.error(info)
+    else:
+        for f in files:
+            os.remove(f)
+    return HttpResponse(info)
+
 
 @csrf_exempt
 def index(request):
@@ -109,37 +41,28 @@ def index(request):
     if not url:
         return HttpResponse("URL needed.")
 
-    if request.POST.get('url') != "HN":
-        mobi_file = save_to_file(url, dir_name=settings.KINDLE_LIVE_DIR)
-        if mobi_file:
-            if not settings.DEBUG:
-                send_mail([settings.MY_KINDLE_MAIL,], "New documentation here", "Sent from mitnk.com", files=[mobi_file,])
-                os.remove(mobi_file)
-            return HttpResponse("%s Sent!" % mobi_file)
-        else:
-            return HttpResponse("No file generated!")
-
-    elif request.POST.get('url') == "HN":
+    if url == "HN":
         hn = HackerNews(fetch=True)
-        count = filed = 0
-        for art in hn.articles:
-            try:
-                news = News.objects.get(url=art['url'])
-                news.points = art['points']
-                news.save()
-            except News.DoesNotExist:
-                news = News(url=art['url'], points=art['points'], title=smart_str(art['title']))
-                news.save()
-                count += 1
+        count_logged, count_filed = News.objects.update_news(hn.articles)
+        return HttpResponse("Find %s news (filed %s).\n" % (count_logged, count_filed))
+    else:
+        if not url.startswith('http'):
+            url = 'http://' + url
 
-            # Save article to file whose points big enough
-            if art['points'] >= settings.POINTS_LIMIT_TO_KINDLE and (not news.filed):
-                mobi = save_to_file(news.url, title=news.title)
-                if mobi:
-                    news.filed = True
-                    news.save()
-                    filed += 1
-                else:
-                    logger.error("Save file failed. URL: %s" % news.url)
-        return HttpResponse("Find %s news (filed %s).\n" % (count, filed))
+        try:
+            br = Briticle(url)
+        except Exception, e:
+            if isinstance(e, URLError) or 'timed out' in str(e):
+                logger.info("URLError or Time out Exception: %s URL: %s" % (e, url))
+                return HttpResponse("Internal Time Out.")
+            raise
+
+        doc_file = br.save_to_file(settings.KINDLE_LIVE_DIR)
+        if doc_file:
+            if not settings.DEBUG:
+                send_mail([settings.MY_KINDLE_MAIL,], "New documentation here", "Sent from mitnk.com", files=[doc_file,])
+                os.remove(doc_file)
+            return HttpResponse("%s Sent!" % doc_file)
+        else:
+            return HttpResponse("Error: No file generated. URL: %s" % url)
     return HttpResponse("NOT 404")
