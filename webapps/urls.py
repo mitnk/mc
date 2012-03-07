12e9c15456341ec206a5d73d752ad31aabb29367
mitnk <whgking@gmail.com>
2012-03-07 16:20:59 +0800

    update URLconf

--- webapps/urls.py
+++ webapps/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'webapps.views.index'),
     (r'^ckeditor/$', 'webapps.views.ckeditor'),
-    (r'^check_website/$', 'webapps.views.check_website'),
     (r'^send_tweets_to_kindle/$', 'webapps.views.send_tweets_to_kindle'),
     (r'^zongheng/', include('webapps.zongheng.urls')),
 )
