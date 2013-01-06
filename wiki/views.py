da350b8d239f5848e049c3c10b28a1a796caefac
mitnk <whgking@gmail.com>
2013-01-07 00:02:08 +0800

    update wiki templates

--- wiki/views.py
+++ wiki/views.py
@@ -3,8 +3,8 @@ from mitnkcom.wiki.models import Article
 
 def index(request):
     articles = Article.objects.order_by("-added")
-    return render(request, 'blog/article_list.html', {'articles': articles})
+    return render(request, 'wiki/article_list.html', {'articles': articles})
 
 def get_article(request, aid):
     article = get_object_or_404(Article, pk=aid)
-    return render(request, 'blog/article.html', {'article': article})
+    return render(request, 'wiki/article.html', {'article': article})
